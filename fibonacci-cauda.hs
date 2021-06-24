8781410f27a838efed70caf382612c72cec0414b
Fabrizio Honda <61120552+fabhonda@users.noreply.github.com>
2021-06-23 22:14:18 -0300

    Add files via upload

---
+++ fibonacci-cauda.hs
@@ -0,0 +1,13 @@
+module Fib(
+   fibcauda
+  ) where
+  
+fibcauda :: Integer -> Integer
+fibcauda n = fibaux 0 1 n
+      where
+        fibaux a b n | n <= 1 = b
+                     | otherwise = fibaux b (a+b) (n-1)
+
+
+
+                     
